134c7508c626bfd0020972c43a49b55d86e726dd
darthReader <ddimitrov10@gmail.com>
2022-07-05 10:59:12 +0300

    first commit

---
+++ src/encapsulation/problem_01/Main.java
@@ -0,0 +1,13 @@
+package encapsulation.problem_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine() + ":");
+        for (Deck value : Deck.values()) {
+            System.out.println("Ordinal value: " + value.ordinal() + "; Name value: " + value.name());
+        }
+    }
+}
