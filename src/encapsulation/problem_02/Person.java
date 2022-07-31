016ffb9628c9b5cc1cb8eb285c95f35adb91cdd4
darthReader <ddimitrov10@gmail.com>
2022-07-31 14:27:02 +0300

    first commit

---
+++ src/encapsulation/problem_02/Person.java
@@ -0,0 +1,51 @@
+package encapsulation.problem_02;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", firstName, lastName, salary);
+    }
+
+    public void increaseSalary(double bonus) {
+        if(this.age < 30) {
+            this.salary = salary + salary * bonus / 200;
+        } else {
+            this.salary = salary + salary * bonus / 100;
+        }
+    }
+}
