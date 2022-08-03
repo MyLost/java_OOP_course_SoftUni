b530783bf45fa7f53f2e87722f08c9b5152a9dda
darthReader <ddimitrov10@gmail.com>
2022-08-03 10:37:27 +0300

    first commit

---
+++ src/interfaces_and_abstraction/say_hello_extend/Bulgarian.java
@@ -0,0 +1,13 @@
+package interfaces_and_abstraction.say_hello_extend;
+
+public class Bulgarian extends BasePerson implements Person {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
