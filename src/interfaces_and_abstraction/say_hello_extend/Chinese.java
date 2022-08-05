67ed513dc4bf3fa259118398139a4b2ec4e88637
darthReader <ddimitrov10@gmail.com>
2022-08-05 13:46:16 +0300

    first commit

---
+++ src/interfaces_and_abstraction/say_hello_extend/Chinese.java
@@ -0,0 +1,13 @@
+package interfaces_and_abstraction.say_hello_extend;
+
+public class Chinese extends BasePerson implements Person {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
