41a337854f9a5f67f3d483a45d9b8d19980df776
darthReader <ddimitrov10@gmail.com>
2022-08-07 23:53:43 +0300

    first commit

---
+++ src/problem_02/Citizen.java
@@ -0,0 +1,35 @@
+package problem_02;
+
+public class Citizen implements Identifiable, Birthable, Person {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDay;
+
+    public Citizen(String name, int age, String id, String birthDay) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+}
