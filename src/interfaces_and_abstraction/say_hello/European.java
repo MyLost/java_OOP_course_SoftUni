4bdc04cff339a15d763f895bf0f5e91df052bc71
darthReader <ddimitrov10@gmail.com>
2022-08-07 23:54:12 +0300

    first commit

---
+++ src/interfaces_and_abstraction/say_hello/European.java
@@ -0,0 +1,20 @@
+package interfaces_and_abstraction.say_hello;
+
+public class European implements Person {
+
+    private String name;
+
+    public European(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
